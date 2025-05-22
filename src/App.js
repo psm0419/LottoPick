642007bb83f224a27845011ad3b17bfd4d935a51
psm0419 <sm8804@naver.com>
2025-05-22 16:17:11 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <div>      
-      {/* <LottoPick />  */}
-      <LottoDownloader />     
+      <LottoPick /> 
+      {/* <LottoDownloader />      */}
     </div>
   );
 }
